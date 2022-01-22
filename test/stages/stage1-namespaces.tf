531379322ab1624bd99ce07220b3b063e7d8b0ca
Thomas Skill <tskill@us.ibm.com>
2022-01-22 16:52:54 -0600

    namespace test change

--- test/stages/stage1-namespaces.tf
+++ test/stages/stage1-namespaces.tf
@@ -2,13 +2,13 @@ module "dev_tools_namespace" {
   source = "github.com/ibm-garage-cloud/terraform-cluster-namespace?ref=v3.1.2"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
-  name                     = "turbonomic"
+  name                     = "tom-turbonomic"
   create_operator_group    = true
 }
 
 
 resource null_resource write_namespace {
   provisioner "local-exec" {
-    command = "echo 'turbonomic' > ${path.cwd}/namespace"
+    command = "echo 'tom-turbonomic' > ${path.cwd}/namespace"
   }
 }
